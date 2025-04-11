f719c2084500944915edf18b4518824e84c696c5
ryan antunes <antunes90000@gmail.com>
2025-04-10 21:02:09 -0300

    exercicios

---
+++ 2029.py
@@ -0,0 +1,15 @@
+while True:
+  try:
+    vol = float(input(""))
+    dia = float(input(""))
+
+    raio = dia/2
+    altura = vol / (3.14 * raio**2)
+    area = 3.14*raio**2
+    altura2 = "{:.2f}".format(altura)
+    area2 = "{:.2f}".format(area)
+    print(f"ALTURA = {altura2}")
+    print(f"AREA = {area2}")
+
+  except EOFError:
+    break
